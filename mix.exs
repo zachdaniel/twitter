ec96c653a3a2e589d3579713132f8e62001f6941
Brian <subsoap@gmail.com>
2023-11-08 23:33:09 -0800

    Update mix.exs
    Update the ecto version to {:ecto, "~> 3.10"} version as the latest OTP and ecto was having the type dynamic in common. so ecto updated the name to dynamic_expr so to fix this issue. either upgrade the ecto or downgrade the OTP.

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Twitter.MixProject do
       # Phoenix
       {:phoenix, "~> 1.7.0-rc.0", override: true},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.10.2"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
